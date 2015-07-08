89a3c04f5425ce8f0916dc3c8c1040dff4d0a69f
vladimir.panarin <vladimir.panarin@exactprosystems.com>
2015-07-08 11:39:32 +0000

    Applied new copyright info.
    svn-rev: 40662

--- src/com/exactprosystems/remotehand/webelements/CssSelector.java
+++ src/com/exactprosystems/remotehand/webelements/CssSelector.java
@@ -1,9 +1,10 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (c) 2009-2015, Exactpro Systems, LLC
-//  Quality Assurance & Related Development for Innovative Trading Systems.
+//  Copyright (c) 2009-2015, Exactpro Systems
+//  Quality Assurance & Related Software Development for Innovative Trading Systems.
+//  London Stock Exchange Group.
 //  All rights reserved.
 //  This is unpublished, licensed software, confidential and proprietary 
-//  information which is the property of Exactpro Systems, LLC or its licensors.
+//  information which is the property of Exactpro Systems or its licensors.
 ////////////////////////////////////////////////////////////////////////////////
 
 package com.exactprosystems.remotehand.webelements;
