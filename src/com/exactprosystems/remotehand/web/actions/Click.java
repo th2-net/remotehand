7391ec5ba7d1affbd998529e5bd739d510d4f840
daria.plotnikova <daria.plotnikova@exactprosystems.com>
2016-03-24 15:15:37 +0000

    Supporting of right-click
    svn-rev: 45108

--- src/com/exactprosystems/remotehand/web/actions/Click.java
+++ src/com/exactprosystems/remotehand/web/actions/Click.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 
 import com.exactprosystems.remotehand.ScriptExecuteException;
 import com.exactprosystems.remotehand.web.WebAction;
@@ -20,6 +21,9 @@ import com.exactprosystems.remotehand.web.WebAction;
 public class Click extends WebAction
 {
 	private static final Logger logger = Logger.getLogger(Click.class);
+	private static final String LEFT = "left", RIGHT = "right", MIDDLE = "middle", BUTTON = "button",
+			X_OFFSET = "xoffset", Y_OFFSET = "yoffset";
+	
 	
 	@Override
 	public boolean isNeedLocator()
@@ -43,8 +47,58 @@ public class Click extends WebAction
 	public String run(WebDriver webDriver, By webLocator, Map<String, String> params) throws ScriptExecuteException
 	{
 		WebElement element = findElement(webDriver, webLocator);
+		
+		String button = params.get(BUTTON);
+		if (button == null)
+			button = LEFT;
+		
 		try {
-			element.click();
+			if (button.equals(LEFT))
+				element.click();
+			else if (button.equals(RIGHT))
+			{
+				String xOffsetStr, yOffsetStr;
+				int xOffset = 0, yOffset = 0;
+				xOffsetStr = params.get(X_OFFSET);
+				yOffsetStr = params.get(Y_OFFSET);
+				
+				Actions actions = new Actions(webDriver);
+				
+				if (xOffsetStr != null && !xOffsetStr.isEmpty() && Y_OFFSET != null && !yOffsetStr.isEmpty())
+				{
+					try
+					{
+						xOffset = Integer.valueOf(xOffsetStr);
+						yOffset = Integer.valueOf(yOffsetStr);
+					}
+					catch (Exception e)
+					{
+						logger.error("xOffset or yOffset is not integer value");
+					}
+					actions.moveToElement(element,xOffset,yOffset).contextClick().perform();
+				}
+				else
+				{
+					actions.contextClick(element).perform();
+				}
+
+				//actions.moveToElement(element,xOffset,yOffset).contextClick().perform();;
+				
+				//actions.contextClick(element);
+				//actions.perform();
+			}
+			else if (button.equals(MIDDLE))
+			{
+				
+			}
+			else
+			{
+				logger.error("Button may be only left, right or middle.");
+				return null;
+			}
+			
+			logger.info("Clicked " + button + " button " + "on: '" + element.toString() + "'");
+				
 		} catch (ElementNotVisibleException e) {
 			logger.error("Element is not visible" ,e);
 
@@ -53,7 +107,7 @@ public class Click extends WebAction
 
 		}
 
-		logger.info("Clicked on: '" + element.toString() + "'");
+		
 
 		return null;
 	}
