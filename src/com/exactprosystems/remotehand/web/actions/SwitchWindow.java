6be4c14f28dfb23f3d9cb8f27be4265d542645de
vladimir.panarin <vladimir.panarin@exactprosystems.com>
2017-05-03 09:21:22 +0000

    Feature #41378 : [RemoteHand] Action to switch to other browser window
    svn-rev: 51334

--- src/com/exactprosystems/remotehand/web/actions/SwitchWindow.java
+++ src/com/exactprosystems/remotehand/web/actions/SwitchWindow.java
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2009-2017, Exactpro Systems
+//  Quality Assurance & Related Software Development for Innovative Trading Systems.
+//  London Stock Exchange Group.
+//  All rights reserved.
+//  This is unpublished, licensed software, confidential and proprietary 
+//  information which is the property of Exactpro Systems or its licensors.
+////////////////////////////////////////////////////////////////////////////////
+
 package com.exactprosystems.remotehand.web.actions;
 
 import java.util.Iterator;
@@ -20,89 +29,95 @@ import com.exactprosystems.remotehand.web.WebAction;
  */
 public class SwitchWindow extends WebAction
 {
-    private final static Logger logger = Logger.getLogger(SwitchWindow.class);
-    private final static String WINDOW = "window";
-
-    public SwitchWindow()
-    {
-        super.mandatoryParams = new String[]{WINDOW};
-    }
-
-    @Override
-    public boolean isNeedLocator()
-    {
-        return false;
-    }
-
-    @Override
-    protected boolean waitForElement(WebDriver webDriver, int seconds, By webLocator) throws ScriptExecuteException
-    {
-        Boolean findWindow;
-        try
-        {
-            findWindow = (new WebDriverWait(webDriver, seconds)).until(new ExpectedCondition<Boolean>() {
-                @Override
-                public Boolean apply(WebDriver driver)
-                {
-                    try
-                    {
-                        return driver.getWindowHandles().size() >= getIntegerParam(getParams(), WINDOW) + 1;
-                    } catch (WebDriverException | ScriptExecuteException e)
-                    {
-                        logger.error("During waiting opened window was thrown exception {}", e);
-                        return false;
-                    }
-                }
-            });
-        } catch (TimeoutException ex)
-        {
-            throw new ScriptExecuteException("Timed out after " + seconds + ". Actual number opened windows is: " + webDriver.getWindowHandles().size() + ". Expected: " + getIntegerParam(getParams(), WINDOW) + 1);
-        }
-
-        if (!findWindow)
-            throw new ScriptExecuteException("Actual number opened windows is: " + webDriver.getWindowHandles().size() + ". Expected: " + getIntegerParam(getParams(), WINDOW) + 1);
-
-        logInfo("Number of windows: '%s'", webDriver.getWindowHandles().size());
-        return findWindow;
-    }
-
-    @Override
-    public boolean isCanWait()
-    {
-        return true;
-    }
-
-    @Override
-    public String run(WebDriver webDriver, By webLocator, Map<String, String> params) throws ScriptExecuteException
-    {
-        Set<String> windowHandles = webDriver.getWindowHandles();
-        Iterator<String> iterator = windowHandles.iterator();
-        if (iterator.hasNext())
-        {
-            int windowNumber = getIntegerParam(params, WINDOW);
-            if (windowNumber < 0 || windowNumber > windowHandles.size() - 1)
-            {
-                String errorMessage = "There is no such window: " + windowNumber;
-                logger.error(errorMessage);
-                throw new ScriptExecuteException(errorMessage);
-            } else
-            {
-                String windowHandle = iterator.next();
-                for (int i = 0; i < windowNumber; i++)
-                {
-                    windowHandle = iterator.next();
-                }
-                webDriver.switchTo().window(windowHandle);
-                logger.debug("Child number is: " + windowNumber);
-                webDriver.switchTo().window(windowHandle);
-            }
-        }
-        return null;
-    }
-
-    @Override
-    protected Logger getLogger()
-    {
-        return logger;
-    }
+	private final static Logger logger = Logger.getLogger(SwitchWindow.class);
+	private final static String WINDOW = "window";
+	
+	public SwitchWindow()
+	{
+		super.mandatoryParams = new String[] { WINDOW };
+	}
+	
+	@Override
+	public boolean isNeedLocator()
+	{
+		return false;
+	}
+	
+	@Override
+	protected boolean waitForElement(WebDriver webDriver, int seconds, By webLocator) throws ScriptExecuteException
+	{
+		final int expectedNumber = getIntegerParam(getParams(), WINDOW) + 1;
+		Boolean findWindow;
+		try
+		{
+			findWindow = (new WebDriverWait(webDriver, seconds)).until(new ExpectedCondition<Boolean>()
+			{
+				@Override
+				public Boolean apply(WebDriver driver)
+				{
+					try
+					{
+						return driver.getWindowHandles().size() >= expectedNumber;
+					}
+					catch (WebDriverException e)
+					{
+						logger.error("Error while waiting for window #"+expectedNumber+" to open", e);
+						return false;
+					}
+				}
+			});
+		}
+		catch (TimeoutException ex)
+		{
+			throw new ScriptExecuteException("Timed out after " + seconds + ". Actual number of open windows is: " + webDriver.getWindowHandles().size() + ". Expected: "
+					+ expectedNumber);
+		}
+		
+		if (!findWindow)
+			throw new ScriptExecuteException("Actual number of open windows is: " + webDriver.getWindowHandles().size() + ". Expected: " + expectedNumber);
+		
+		logInfo("Number of open windows: '%s'", webDriver.getWindowHandles().size());
+		return findWindow;
+	}
+	
+	@Override
+	public boolean isCanWait()
+	{
+		return true;
+	}
+	
+	@Override
+	public String run(WebDriver webDriver, By webLocator, Map<String, String> params) throws ScriptExecuteException
+	{
+		Set<String> windowHandles = webDriver.getWindowHandles();
+		Iterator<String> iterator = windowHandles.iterator();
+		if (iterator.hasNext())
+		{
+			int windowNumber = getIntegerParam(params, WINDOW);
+			if (windowNumber < 0 || windowNumber > windowHandles.size() - 1)
+			{
+				String errorMessage = "There is no such window: " + windowNumber;
+				logger.error(errorMessage);
+				throw new ScriptExecuteException(errorMessage);
+			}
+			else
+			{
+				String windowHandle = iterator.next();
+				for (int i = 0; i < windowNumber; i++)
+				{
+					windowHandle = iterator.next();
+				}
+				webDriver.switchTo().window(windowHandle);
+				logger.debug("Child number is: " + windowNumber);
+				webDriver.switchTo().window(windowHandle);
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	protected Logger getLogger()
+	{
+		return logger;
+	}
 }
