611acc9c0b6ca2177f1f89f8313f5f3194e2a425
vladimir.panarin <vladimir.panarin@exactprosystems.com>
2019-05-15 13:25:52 +0000

    Developer note: formatting fixed.
    svn-rev: 65559

--- src/com/exactprosystems/remotehand/web/actions/GetElementValue.java
+++ src/com/exactprosystems/remotehand/web/actions/GetElementValue.java
@@ -1,3 +1,13 @@
+/******************************************************************************
+ * Copyright (c) 2009-2019, Exactpro Systems LLC
+ * www.exactpro.com
+ * Build Software to Test Software
+ *
+ * All rights reserved.
+ * This is unpublished, licensed software, confidential and proprietary 
+ * information which is the property of Exactpro Systems LLC or its licensors.
+ ******************************************************************************/
+
 package com.exactprosystems.remotehand.web.actions;
 
 import com.exactprosystems.remotehand.ScriptExecuteException;
@@ -8,9 +18,9 @@ import java.util.Map;
 
 public class GetElementValue extends GetElement
 {
-    @Override
-    public String run(WebDriver webDriver, By webLocator, Map<String, String> params) throws ScriptExecuteException
-    {
-        return findElement(webDriver, webLocator).getAttribute("value");
-    }
+	@Override
+	public String run(WebDriver webDriver, By webLocator, Map<String, String> params) throws ScriptExecuteException
+	{
+		return findElement(webDriver, webLocator).getAttribute("value");
+	}
 }
