319bff7baefacd316920c1f4eaa9c8e820321e24
daniel.samatov <daniel.samatov@exactprosystems.com>
2020-08-31 18:47:14 +0300

    Bug #50821. [LCH regression] capi stopped answering to the rest requests Session watcher will not be created if session expire time is less or equal to 0

--- src/main/java/com/exactprosystems/remotehand/sessions/SessionWatcher.java
+++ src/main/java/com/exactprosystems/remotehand/sessions/SessionWatcher.java
@@ -32,6 +32,12 @@ public class SessionWatcher implements Runnable
 
 	public static SessionWatcher getWatcher()
 	{
+		if (SESSION_EXPIRE_IN_MINUTES < 1)
+		{
+			logger.info("Session watcher is not created: session expiry time less or equal to 0");
+			return null;
+		}
+		
 		SessionWatcher localInstance = watcher;
 		if (localInstance == null)
 		{
@@ -48,13 +54,7 @@ public class SessionWatcher implements Runnable
 	@Override
 	public void run()
 	{
-		if (SESSION_EXPIRE == 0)
-		{
-			logger.info("Session watcher is not executed because session expiry time equals 0");
-			return;
-		}
-
-		logger.info("Session watcher is executed. SessionExpire (min) = " + SESSION_EXPIRE_IN_MINUTES);
+		logger.info("Session watcher thread is running. Session expire time: {} min", SESSION_EXPIRE_IN_MINUTES);
 
 		while (true)
 		{
@@ -65,12 +65,12 @@ public class SessionWatcher implements Runnable
 			}
 			catch (InterruptedException e)
 			{
-				// do nothing
+				Thread.currentThread().interrupt();
 			}
 		}
 	}
 
-	public long closeSessionIfTimeOver()
+	private long closeSessionIfTimeOver()
 	{
 		long timeToNextSessionEnd = SESSION_EXPIRE;
 		synchronized (timeSessions)
@@ -89,9 +89,8 @@ public class SessionWatcher implements Runnable
 					SessionHandler session = entry.getKey();
 					if (session != null)
 					{
-						logger.warn("Session " + session.getId() + " is inactive more than " + 
-								SESSION_EXPIRE_IN_MINUTES + " minutes. " +
-								"It will be closed due to timeout");
+						logger.warn("Session {} is inactive more than {} minutes. It will be closed due to timeout",
+								session.getId(), SESSION_EXPIRE_IN_MINUTES);
 						session.close();
 						iterator.remove();
 					}
@@ -103,7 +102,15 @@ public class SessionWatcher implements Runnable
 		return timeToNextSessionEnd;
 	}
 
-	public void addSession(SessionHandler session)
+	public static void watchSession(SessionHandler session)
+	{
+		if (watcher != null)
+			watcher.addSession(session);
+		else
+			logger.trace("Unable to watch session: session watcher was not created");
+	}
+	
+	private void addSession(SessionHandler session)
 	{
 		synchronized (timeSessions)
 		{
@@ -112,7 +119,15 @@ public class SessionWatcher implements Runnable
 		}
 	}
 	
-	public void updateSession(SessionHandler session)
+	public static void updateSession(SessionHandler session)
+	{
+		if (watcher != null)
+			watcher.updateSessionTime(session);
+		else
+			logger.trace("Unable to update session: session watcher was not created");
+	}
+	
+	private void updateSessionTime(SessionHandler session)
 	{
 		synchronized (timeSessions)
 		{
